1b9a248bad16e8ac9f732abfb588d28fa2741ccf
Vrath <xvrath@gmail.com>
2023-07-05 21:22:55 +0200

    0.0.2 - save/load/reset, upgrades

--- src/tabs/Foundry.vue
+++ src/tabs/Foundry.vue
@@ -1,17 +1,26 @@
 <script setup> 
+import {golemTypes} from '../scripts/foundry.js'
 
 </script>
 <template>
   <!-- <h2 class="decorated"><span>production</span></h2> -->
   <div class="center-stuff">
-    
+    <div class="card" v-for="g in golemTypes">
+      <div class="card-content">
+        <h2 class="card-title">
+          {{ g.displayName }} 
+        </h2>
+
+        
+      </div>
+      
+
+
+    </div>
+
+
   </div>
 </template>
-<script lang="ts">
-
-
-
-</script>
 <style lang="">
   
 </style>
